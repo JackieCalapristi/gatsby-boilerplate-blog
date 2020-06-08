0b0c9dcba0eb9c34ec77ed78c06f56a6ad9ae760
JackieCalapristi <jackie.lc11@gmail.com>
2020-06-07 19:30:00 -0700

    Saving Spot

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,11 +7,11 @@ import { Nav } from "./Menu.styles"
 const Menu = ({ items }) => (
   <Nav>
     {items.map(item => (
-        <li key={item.id}> 
-          <Link to={item.link} activeClassName="active">
-            {item.name} 
-          </Link>
-        </li>
+      <li key={item.id}> 
+        <Link to={item.link} activeClassName="active">
+          {item.name} 
+        </Link>
+      </li>
     ))}
   </Nav>
 );
