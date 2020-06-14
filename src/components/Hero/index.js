7ceb868165ae83b8cbf48b06254c0e134c2a79cc
JackieCalapristi <jackie.lc11@gmail.com>
2020-06-14 13:25:15 -0700

    Adding Hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "gatsby";
+import { duplicateVariableMessage } from "graphql/validation/rules/UniqueVariableNames";
+// Components
+// Hooks
+// Styles
 
-const Hero = () => <div>hero</div>;
+const Hero = () => {
+
+  return (
+    <div>
+      <div>
+        BG Image
+      </div>
+      <div>
+        <h2>title</h2>
+        <Link to ="/">
+          <button>Click Me</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
