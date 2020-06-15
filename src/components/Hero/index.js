2e054ce67d3a65976ac570a65fb0b6cdafd25fb1
JackieCalapristi <jackie.lc11@gmail.com>
2020-06-15 11:51:11 -0700

    create hero query

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,19 +3,26 @@ import { Link } from "gatsby";
 import { duplicateVariableMessage } from "graphql/validation/rules/UniqueVariableNames";
 // Components
 // Hooks
+import { useHeroQuery } from "hooks/useHeroQuery"
 // Styles
 
 const Hero = () => {
-
+  const { 
+    heroImage, 
+    heroImageText, 
+    heroImageBtnText, 
+    heroImageBtnLink 
+  } = useHeroQuery();
+  console.log(heroImage);
   return (
     <div>
       <div>
         BG Image
       </div>
       <div>
-        <h2>title</h2>
-        <Link to ="/">
-          <button>Click Me</button>
+        <h2>{heroImageText}</h2>
+        <Link to={heroImageBtnLink}>
+          <button>{heroImageBtnText}</button>
         </Link>
       </div>
     </div>
