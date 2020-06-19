f7d9c6daea9b0d633497942f2c29f1a1c9b6965c
JackieCalapristi <jackie.lc11@gmail.com>
2020-06-19 15:12:10 -0700

    Adding BlogPostCard

--- src/components/BlogPostCard/index.js
+++ src/components/BlogPostCard/index.js
@@ -1,5 +1,33 @@
 import React from "react";
+import Img from "gatsby-image"
+import { Link } from "gatsby"
+// Icons
+import DateIcon from "images/calendar.svg"
+import TimeIcon from "images/watch.svg"
 
-const BlogPostCard = () => <div>BlogPostCard</div>;
+const BlogPostCard = ({ slug, title, date, readingTime, excerpt, image }) => (
+  <CardWrapper>
+    <ImgWrapper>
+      <Img fluid={image} />
+    </ImgWrapper>
+    <TextWrapper>
+      <Link to={slug}>
+        <h2>{title}</h2>
+      </Link>
+      <StatWrapper>
+        <DateWrapper>
+          <img src={DateIcon} alt="date" />
+          {date}
+        </DateWrapper>
+        <ReadingTimeWrapper>
+          <img src={TimeIcon} alt="time" />
+          {readingTime}
+        </ReadingTimeWrapper>
+      </StatWrapper>
+      <p>{excerpt}</p>
+    </TextWrapper>
+    
+  </CardWrapper>
+)
 
 export default BlogPostCard;
