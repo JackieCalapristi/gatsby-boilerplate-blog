48aaac8073d11f5db6c717f6420b73ea80f63ed9
JackieCalapristi <jackie.lc11@gmail.com>
2020-06-26 12:44:37 -0700

    Adding slug logic for blog images

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,25 @@
 const path = require("path")
 
+const { createFilePath } = require("gatsby-source-filesystem") 
+
+// First create the slug for posts and pages
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `content`
+    })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
+
 // For absolute imports
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
