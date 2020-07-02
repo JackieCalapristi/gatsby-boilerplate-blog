9ae39714fd241b37138046b1e1732795521f2614
JackieCalapristi <jackie.lc11@gmail.com>
2020-07-02 16:26:21 -0700

    Adding page navigation links

---
+++ src/components/PageNavigation/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "gatsby"
+// Icons
+import LeftIcon from "images/arrow-left.svg"
+import RightIcon from "images/arrow-right.svg"
+//Styles
+
+const PageNavigation = ({ currentPage, numPages }) => (
+  <div>
+    <div>
+      {currentPage > 1 && (
+        <Link to ={`/${currentPage === 2 ? '' : currentPage - 1}`} >
+          <input type="image" alt="left-nav" src={LeftIcon} />
+        </Link>
+      )}
+    </div>
+    <div>
+      {currentPage} / {numPages}
+    </div>
+    <div>
+    {currentPage !== numPages && (
+        <Link to ={`/${currentPage + 1}`} >
+          <input type="image" alt="right-nav" src={RightIcon} />
+        </Link>
+      )}
+    </div>
+  </div>
+)
+
+export default PageNavigation
